c6c06daad737a8f84dd153a27aeb9106da9943b4
listen <listen@listendemacbook-pro.local>
2018-03-28 17:18:48 +0800

    开始开发

--- weibo/weibo/ViewController.m
+++ weibo/weibo/ViewController.m
@@ -17,13 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+
+
+    NSLog(@"开始开发");
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 
 @end
